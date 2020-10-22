a7aefa6065ccb5313f9a2f1b259375c120f2e784
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2020-10-21 19:17:34 -0500

    made footer transparent and added logo to the footer message

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import '../Styles/Footer.css';
+import React from "react";
+import "../Styles/Footer.css";
 
 function Footer() {
   return (
     <>
       <footer className="footer py-3">
         <div className="container footerContain">
-          <span className="text-muted footerText">Made by BrockBatBlue with 💕</span>
+          <span className="text-muted footerText">
+            Made by{" "}
+            <img id="footerLogo" src="portfolioLogoOne_200x200.png" alt="" />{" "}
+            with 💚
+          </span>
         </div>
-    </footer>
+      </footer>
     </>
   );
 }
