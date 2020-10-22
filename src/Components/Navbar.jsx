341e6ca533e103008a567d13419f8513ac78a1fe
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2020-10-21 20:03:04 -0500

    cropped logo heights and shortened the width of the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <nav className="navigationBar navbar navbar-expand navbar-light bg-light">
       <Link to="/" className="navbar-brand">
-        <img src="portfolioLogoOne_200x200.png" alt="" />
+        <img src="portfolioLogoTwo.png" alt="" />
       </Link>
       <div id="navbarNav">
         <ul className="navbar-nav">
@@ -19,7 +19,7 @@ function Navbar() {
                 location.pathname === "/about" ? "nav-link active" : "nav-link"
               }
             >
-              <img src="logoPortfolio_200x200.png" alt="" />
+              <img src="logoPortfolioTwo.png" alt="" />
             </Link>
           </li>
           {/* <li className="nav-item">
